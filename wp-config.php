2d1bb12584578aeb68dc78dace08d19b29c6caf7
khanr <khanriza@gmail.com>
2025-10-06 19:55:30 -0400

    refactor

--- wp-config.php
+++ wp-config.php
@@ -18,22 +18,11 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 define('DB_NAME', getenv('DB_NAME') ?: 'wordpress');
-
-/** Database username */
 define('DB_USER', getenv('DB_USER') ?: 'root');
-
-/** Database password */
 define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'password');
-
-/** Database hostname */
 define('DB_HOST', getenv('DB_HOST') ?: 'db');
-
-/** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
-
-/** The database collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
 /**#@+
